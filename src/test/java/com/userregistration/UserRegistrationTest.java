c63d72742d1afe919e41798819d56f5a63dbca45
Minal <patilminalp@gmail.com>
2021-07-08 20:17:28 +0530

    Added:Check Whether First Name valid or not using testCases

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -1,5 +1,29 @@
 package com.userregistration;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+	UserRegistration userRegistration;
+
+	@Before
+	public void setUp() {
+		userRegistration = new UserRegistration();
+	}
+
+	@Test
+	public void givenName_WhenFirstNameValid_ShouldReturnTrue() {
+
+		boolean actual = userRegistration.validateFirstName("Minal");
+		Assert.assertTrue(actual);
+	}
+
+	@Test
+	public void givenName_WhenFirstNameIsLowerCase_ShouldReturnFalse() {
+
+		boolean actual = userRegistration.validateFirstName("minal");
+		Assert.assertFalse(actual);
+	}
 
 }
